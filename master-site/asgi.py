d52ed17c15d88b5ea27dc45dec169a7186600b8f
jiversivers <iversjesse@gmail.com>
2025-01-15 16:59:44 -0600

    updated name refs following rename

--- master-site/asgi.py
+++ master-site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MySite project.
+ASGI config for master-site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MySite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'master-site.settings')
 
 application = get_asgi_application()
