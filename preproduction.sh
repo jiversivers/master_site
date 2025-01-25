7ee412b89c058cd3ac28f527cd4f47aa77be746d
jiversivers <iversjesse@gmail.com>
2025-01-25 10:45:20 -0600

    filled out production prep scripts

--- preproduction.sh
+++ preproduction.sh
@@ -1,4 +1,7 @@
-conda env export --name jiversivers_env > environment.yml
-pip freeze
-git push -m 'preproduction push'
-scp -i ~/jivers.pem .env ubunt fu@ec2-3-145-158-153.us-east-2.compute.amazonaws.com:~/master-site
+#!/bin/bash
+EC2=ubuntu@ec2-3-145-158-153.us-east-2.compute.amazonaws.com
+conda env export --name jiversivers_env > environment.yml # export environment for easy re-creation on EC2
+pip list --format=freeze > requirments.txt  # capture pip reqs in the case that they are not properly handled by conda
+git push -m 'preproduction push'  # getting production onto git hub for easy access on EC2
+scp -i ~/jivers.pem .env "$EC2":~/master-site #pushing .env to remote
+ssh ~/jivers.pem "$EC2" 'bash -s' < production_setup.sh # Run production_setup on EC2 instance
--- production_setup.sh
+++ production_setup.sh
@@ -1,17 +1,45 @@
+#!/bin/bash
 
-git clone --branch production --depth 1 --single-branch https://www.github.com/jiversivers/master-site.git
-sudo apt update && sudo apt upgrade -y
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
+{ # try
+  cd master-site
+} || { # except
+  git clone --branch production --depth 1 --single-branch https://www.github.com/jiversivers/master-site.git
+  cd master-site
+}
 
-sudo apt install nginx -y
+# Install conda if not already
+command -v conda > /dev/null 2>&1
+if [$? -ne 0]; then
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  bash Miniconda3-latest-Linux-x86_64.sh
+fi
+
+# Install nginx if not
+command -v nginx > /dev/null 2>&1
+if [$? -ne 0]; then
+  sudo apt install nginx -y
+fi
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
+# Install to read yaml
+sudo apt install yq
 
+# Get linux up-to-date
+sudo apt update && sudo apt upgrade -y
 
+# Delete environment if exists
+ENV_NAME=$(yq -r .name environment.yml)
+conda env list | grep "$ENV_NAME" >/dev/null 2>&1
+if [$? -ne 0]
+  conda remove -n "$ENV_NAME" --all
+fi
+
+# Create fresh environment
 conda env create -f environment.yml
-pip install -r requirements.txt
 conda activate your_env_name
+pip install -r requirements.txt
 
+# Prep django
 python manage.py migrate
+pyhton manage.py collectstatic
