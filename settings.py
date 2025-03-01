7ecfc0d04dc7a77df9c6cb9b8d40b8d4ec1ef93f
jiversivers <iversjesse@gmail.com>
2025-03-01 13:30:19 -0600

    updated static pointers

--- settings.py
+++ settings.py
@@ -138,11 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_URL = '/project_static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'project_static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
