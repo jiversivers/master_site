39a51fb51fa2b52617ee43889ccaed11c1bc9d71
jiversivers <iversjesse@gmail.com>
2025-03-02 06:26:26 -0600

    clarified static naming

--- settings.py
+++ settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'collected_static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'project_static',
