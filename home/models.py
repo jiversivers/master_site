5d01f892942ffc06423e8a98a356bb8e18511f54
jiversivers <iversjesse@gmail.com>
2025-03-02 12:32:40 -0600

    cleared not null constraint

--- home/models.py
+++ home/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class VisitorMetadata(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     ip_addr = models.GenericIPAddressField()
-    referer = models.TextField()
+    referer = models.TextField(null=True, blank=True)
     path = models.TextField()
