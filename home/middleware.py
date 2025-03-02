1ce38ca1c9753d04a730dc04279944f3e23df2f8
jiversivers <iversjesse@gmail.com>
2025-03-02 12:58:46 -0600

    added visit tracking robustness

--- home/middleware.py
+++ home/middleware.py
@@ -5,7 +5,7 @@ from home.models import VisitorMetadata
 
 class TrackVisit(MiddlewareMixin):
     def process_request(self, request):
-        visit = VisitorMetadata(ip_addr=request.META.get('REMOTE_ADDR', None),
+        visit = VisitorMetadata(ip_addr=request.META.get('HTTP_X_FORWARDED_FOR', '').split(',')[0] or request.META.get('REMOTE_ADDR', 'Unknown'),
                                 referer=request.META.get('HTTP_REFERER', None),
                                 path=request.get_full_path())
         visit.save()
